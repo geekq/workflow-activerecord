fe8eb3b31314d833573557b6b4eebb8a07cddf5e
Vladimir Dobriakov <vladimir.dobriakov@dm.de>
2019-04-04 18:23:38 +0200

    gh-4 Simplify 'require's
    * ruby-graphviz is not needed by workflow-activerecord
    * workflow/draw functionality for documentation is optional and
      better referenced from application's Rakefile like
      https://github.com/geekq/workflow-rails-sample/commit/149e7a9ef5ecbffbc188aff28c96c9b81a82c3c1

--- lib/workflow_activerecord.rb
+++ lib/workflow_activerecord.rb
@@ -3,14 +3,6 @@ require 'rubygems'
 require 'workflow/specification'
 require 'workflow_activerecord/adapters/active_record'
 
-begin
-  require 'ruby-graphviz'
-  require 'active_support/inflector'
-  require 'workflow/draw'
-rescue LoadError => e
-  $stderr.puts "Could not load the ruby-graphiz or active_support gems for rendering: #{e.message}"
-end
-
 module WorkflowActiverecord
   def self.included(klass)
     klass.send :include, ::Workflow
